50d13d76e1972664440abad59d8a3617414c64bb
Jason Hilton <jason_hilton@yahoo.com>
2017-05-06 20:56:02 +0100

    added initial (failing) error handling tests

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(ggfan)
+
+test_check("ggfan")
---
+++ tests/testthat/test_calc_quantile.R
@@ -0,0 +1,29 @@
+context("calc_quantile")
+
+
+test_that("calc_quantile returns df with x and y",
+  {
+  intervals <- c(0.5,0.8)
+  df <- data.frame(x=rep(1:10,5), y=rnorm(50))
+  df_q <- calc_quantiles(df, intervals)
+  expect_true(all(c("x","y") %in% names(df_q)))
+  }
+)
+
+test_that("calc_quantile allows retention of original df names",
+  {
+  intervals <- c(0.5,0.8)
+  df <- data.frame(a=rep(1:10,5), b=rnorm(50))
+  df_q <- calc_quantiles(df, intervals, rename=F)
+  expect_true(all(c("a","b") %in% names(df_q)))
+  }
+)
+
+test_that("calc_quantile returns error when too few observations are present",
+  {
+  intervals <- c(0.5,0.8)
+  df <- data.frame(x=rep(1:10,2), y=rnorm(20))
+  expect_error(df_q <- calc_quantiles(df, intervals))
+  }
+)
+
