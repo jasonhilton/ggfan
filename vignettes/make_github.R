a344ba82807ab5d58595754ea9688f0f444ea670
Jason Hilton <jason_hilton@yahoo.com>
2017-05-28 23:17:44 +0100

    add stan vignette

--- vignettes/make_github.R
+++ vignettes/make_github.R
@@ -1,1 +1,2 @@
 rmarkdown::render(input = "vignettes/geom_fan.Rmd",output_format = rmarkdown::github_document())
+rmarkdown::render(input = "vignettes/ggfan_stan.Rmd",output_format = rmarkdown::github_document())
