b30def7e637300121bf98fe3ab90c456d2a5a455
Jason Hilton <jason_hilton@yahoo.com>
2017-05-28 15:57:39 +0100

    changed path to vignette in github script

--- vignettes/make_github.R
+++ vignettes/make_github.R
@@ -1,1 +1,1 @@
-rmarkdown::render(input = "geom_fan.Rmd",output_format = rmarkdown::github_document())
+rmarkdown::render(input = "vignettes/geom_fan.Rmd",output_format = rmarkdown::github_document())
